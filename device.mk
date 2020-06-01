17596b2ed81766fbeeb5e26f46cb1b4a93d34d80
Alexander Koskovich <zvnexus@outlook.com>
2020-06-01 02:29:06 +0000

    Poco F2 Pro / Redmi K30 Pro / Redmi K30 Pro Zoom Edition
     * A bit of documentation so people have context if this
       is rebased for future devices.
    
     * Fstab was taken from LA.UM.9.11.r1-02200-NICOBAR.0. It's located in
       vendor but we have it here for recovery and second stage init.
    
     * This device tree is for devices with prebuilt vendors.
    
     * Kernel cmdline was taken from LA.UM.9.11.r1-02200-NICOBAR.0
       with the exception of video=*.
    
     * BOARD_AVB_RECOVERY_KEY_PATH must be defined if non-A/B is supported.
       See https://android.googlesource.com/platform/external/avb/+/master/README.md#booting-into-recovery.
    
    Change-Id: Ia48812a5999caffa7fa185a2292d3ce1ab29dbb9

---
+++ device.mk
@@ -0,0 +1,36 @@
+# Copyright (C) 2020 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# APEX
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Boot
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl-qti.recovery
+
+# GSI
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
+# Namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    device/xiaomi/lmi
+
+# Shipping API
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Update Engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
