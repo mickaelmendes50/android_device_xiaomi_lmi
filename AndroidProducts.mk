3acff6a9ee09a7b94a3c21fa8c7a8e71bac72118
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-10-23 09:35:08 +0200

    lmi: Inherit from common tree
    Change-Id: Icaf0e5cb7dbb8c75b81bf9008433ab51a9b34531

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_lmi.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_lmi-user \
+    lineage_lmi-userdebug \
+    lineage_lmi-eng
---
+++ BoardConfig.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sm8250-common
+include device/xiaomi/sm8250-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/lmi
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
+
+# Display
+TARGET_SCREEN_DENSITY := 440
+
+# Fingerprint
+SOONG_CONFIG_XIAOMI_KONA_FOD_POS_X = 439
+SOONG_CONFIG_XIAOMI_KONA_FOD_POS_Y = 1655
+SOONG_CONFIG_XIAOMI_KONA_FOD_SIZE = 202
+
+# Kernel
+TARGET_KERNEL_CONFIG := vendor/lmi_defconfig
+
+# Inherit from the proprietary version
+include vendor/xiaomi/lmi/BoardConfigVendor.mk
