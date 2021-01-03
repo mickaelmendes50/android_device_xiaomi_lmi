6578b87516cfaf8c739f1e4300bba600453573ab
Alexander Koskovich <zvnexus@outlook.com>
2021-01-03 12:20:35 -0300

    lmi: Initial support for build a vendor partition
     * This tree tries to stay as inline with CAF as possible
       and only starts inserting bits from stock when it
       impacts functionality to a noticeable extent.
    
    Change-Id: Icfed06e8e5bafb6b1f359ea39508294430129c1c
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- device.mk
+++ device.mk
@@ -17,11 +17,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Boot
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti.recovery
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-service
+
+# Config Store
+PRODUCT_PACKAGES += \
+    disable_configstore
 
 # Fastboot
 PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
     fastbootd
+
+# Gatekeeper
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.gatekeeper.disable_spu=true
 
 # GSI
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
@@ -35,10 +46,15 @@ PRODUCT_SOONG_NAMESPACES += \
     device/xiaomi/lmi
 
 # NFC
+TARGET_USES_NQ_NFC := true
+
+$(call inherit-product, vendor/nxp/opensource/commonsys/packages/apps/Nfc/nfc_system_product.mk)
+$(call inherit-product, vendor/nxp/opensource/halimpl/nfc_vendor_product.mk)
+
 PRODUCT_PACKAGES += \
-    NfcNci \
-    Tag \
-    SecureElement
+    se_nq_extn_client \
+    ls_nq_client \
+    jcos_nq_client
 
 # Overlays
 PRODUCT_PACKAGES += \
@@ -50,6 +66,10 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Platform
 TARGET_BOARD_PLATFORM := kona
+
+# QSPM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qspm.enable=true
 
 # QTI
 TARGET_COMMON_QTI_COMPONENTS += \
@@ -68,6 +88,10 @@ TARGET_COMMON_QTI_COMPONENTS += \
     wfd \
     wlan
 
+# Recovery
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.recovery.ui.margin_height=150
+
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
@@ -79,3 +103,7 @@ PRODUCT_PACKAGES += \
 
 # Vendor
 $(call inherit-product, vendor/xiaomi/lmi/lmi-vendor.mk)
+
+# Verified Boot
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
