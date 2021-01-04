c13bc15e3032cea80f9a341b04348a3c8b49cc05
Alexander Koskovich <zvnexus@outlook.com>
2021-01-04 12:10:01 -0300

    lmi: Use the QTI health implementation.
    Change-Id: I1a9fee97d9cfae7f47a2218441676b8aaa271516
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- device.mk
+++ device.mk
@@ -86,6 +86,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # GSI
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl-qti \
+    android.hardware.health@2.1-service
+
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
