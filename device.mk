cc7d1af6f19ba686edbecc893a9721dfc55e03d2
Alexander Koskovich <zvnexus@outlook.com>
2021-03-20 19:42:15 -0700

    lmi: Build the QTI thermal HAL.
    Change-Id: Ie9c3640bf48c81fab359d26411e9390dc54a57cd

--- device.mk
+++ device.mk
@@ -150,6 +150,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # Vendor Service Manager
 PRODUCT_PACKAGES += \
     vndservicemanager
