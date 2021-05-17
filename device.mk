e1cfa9ba92153221f289980cce5560c5eb494857
Mesquita <mesquita@aospa.co>
2021-05-17 08:13:57 -0300

    lmi: overlay: Switch overlays to RRO
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- device.mk
+++ device.mk
@@ -5,12 +5,11 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pa
-
 PRODUCT_PACKAGES += \
-    AOSPALmiFrameworks
+    AOSPALmiFrameworks \
+    AOSPALmiSystemUI \
+    LmiFrameworks \
+    LmiSystemUI
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
