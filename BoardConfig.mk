1ec2b533eb6d13e09c5024997911d4188ceacd3e
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-06-23 19:00:50 +0530

    lmi: Add xiaomi specific vintf configuration
    Change-Id: I1ad5f39c1288714c6a27ae254b8568783c0fb815

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,8 +60,12 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_SCREEN_DENSITY := 440
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/xiaomi_vendor_framework_compatibility_matrix.xml
+
 DEVICE_MANIFEST_FILE += \
-    $(DEVICE_PATH)/configs/vintf/manifest.xml
+    $(DEVICE_PATH)/configs/vintf/manifest.xml \
+    $(DEVICE_PATH)/configs/vintf/xiaomi_manifest.xml
 
 DEVICE_MATRIX_FILE += \
     device/qcom/common/compatibility_matrix.xml
