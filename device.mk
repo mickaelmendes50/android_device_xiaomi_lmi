5bac8dfc685f0de5a8a51c6c40be904ca4324c3c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-24 23:26:36 +0800

    lmi: biometrics: Rebrand to lmi
    Change-Id: If1106b9567ec5e138e1404dc49f99d63f7fb6c84
    Signed-off-by: kubersharma001 <kubersharma001@gmail.com>

--- device.mk
+++ device.mk
@@ -191,7 +191,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service.lmi
 
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
