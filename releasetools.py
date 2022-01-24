fd27ae2f33af04beae3517fd739d9bd6ccc41ded
Ramii Ahmed <ramy@ahmedramy.com>
2022-01-24 21:29:33 +0200

    lmi: releasetools: flash recovery as well
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- releasetools.py
+++ releasetools.py
@@ -36,6 +36,7 @@ def AddImage(info, basename, dest):
 
 def OTA_InstallEnd(info):
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+  AddImage(info, "recovery.img", "/dev/block/bootdevice/by-name/recovery")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
 
