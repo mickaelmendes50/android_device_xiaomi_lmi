mesziman/android_device_xiaomi_lmi-5

lmi: init: Add xiaomi specific init modifications
Change-Id: Ic846d98e400c8a519598ec6f2bac1ca30b2e1dd5

device.mk
@@ -209,8 +209,11 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    init.lmi.rc \
+    init.mi.usb.sh \
     init.qti.dcvs.sh \
-    init.target.rc
+    init.target.rc \
+    ueventd.lmi.rc
 
 # Keymaster
 PRODUCT_PROPERTY_OVERRIDES += \

mickaelmendes50/android_device_xiaomi_lmi

device.mk
@@ -209,8 +209,11 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    init.xiaomi.rc \
+    init.mi.usb.sh \
     init.qti.dcvs.sh \
-    init.target.rc
+    init.target.rc \
+    ueventd.xiaomi.rc
 
 # Keymaster
 PRODUCT_PROPERTY_OVERRIDES += \