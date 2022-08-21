8a95e2d6ab5d28a7bbea19ace548839fb7248c89
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-08-21 08:34:51 +0000

    lmi: Define AAPT configuration
    Change-Id: Ic12fc243a6b86a0972ef5dc0bb26148be540be57
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,10 @@
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Attestation
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
