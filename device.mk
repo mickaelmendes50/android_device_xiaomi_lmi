67b7277c2095eba4856d5d1f9efb36f293c0b330
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-07-08 15:14:37 -0300

    lmi: Build Xiaomi sensors impl
    Change-Id: Ib477ac016c312ce02e9f71df2a7d1d0fcca717e8

--- device.mk
+++ device.mk
@@ -287,7 +287,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-impl-xiaomi \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge \
     sensors.udfps
